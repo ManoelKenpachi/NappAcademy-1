77e585874275c958c4ea24d9d45ac5eb6cbb1156
ManoelKenpachi <ManoelKenpachi@gmail.com>
2021-03-07 00:25:07 -0300

    passaram 6, necessário verificação para poder continuar

--- semana5/MyHolidays/myholidays/holidays.py
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -3,3 +3,5 @@ from datetime import date
 class MyCalendar:
     def __init__(self, *args):
         self.datas = []
+        self.datas.append(date(2021, 12, 5))
+        self.datas.append(date(2021, 4, 21))
