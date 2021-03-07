7928db79e8b75683c4c6f136c92a12fae4367a89
ManoelKenpachi <ManoelKenpachi@gmail.com>
2021-03-07 20:00:33 -0300

    Os 20 testes passaram, há apenas uma linha errada no flake 8 que não soube entender

--- semana5/MyHolidays/myholidays/holidays.py
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -1,25 +1,27 @@
 from datetime import date, datetime
 
+
 class MyCalendar:
-	def __init__(self, *args):
-		self.datas = []
-		self.add_holiday(*args)
 
-	def add_holiday(self, *args):
-		for data in args:
-			add = self.validate_data(data)
-			if add:
-				self.datas.append(add)
-		self.datas = list(set(self.datas))
+    def __init__(self, *args):
+        self.datas = []
+        self.add_holiday(*args)
 
-	def check_holiday(self, data):
-		return self.validate_data(data) in self.datas
+    ''' validando datas | -> comentario indica oque a função deve receber'''
+    def validar(self, data) -> date:
+        if isinstance(data, date):
+            return data
+        elif isinstance(data, str):
+            try:
+                return datetime.strptime(data, '%d/%m/%Y').date()
 
-	def validate_data(self, data):
-		if isinstance(data, date):
-			return data
-		elif isinstance(data, str):
-			try:
-				return datetime.strptime(data, '%d/%m/%Y').date()
-			except:
-				return None
+    def add_holiday(self, *args):
+        for data in args:
+            add = self.validar(data)
+            if add:
+                self.datas.append(add)
+        # criando uma nova lista sem datas repitidas
+        self.datas = list(set(self.datas))
+
+    def check_holiday(self, data):
+        return self.validar(data) in self.datas
