846597c374d5344d0f999eb9ef0539acbde9dd47
ManoelKenpachi <ManoelKenpachi@gmail.com>
2021-03-07 19:19:03 -0300

    18, 19 e 20 falharam

--- semana5/MyHolidays/myholidays/holidays.py
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -3,12 +3,15 @@ from datetime import date, datetime
 class MyCalendar:
 	def __init__(self, *args):
 		self.datas = []
-	#	self.add_holiday(*args)
+		self.add_holiday(*args)
 
+	def add_holiday(self, *args):
+		for data in args:
+			d = self.validate_data(data)
+			if d:
+				self.datas.append(d)
+		self.datas = list(set(self.datas))
 
-
-	def check_holiday(self, data):
-		return self.validate_data(data) in self.datas
 
 	def validate_data(self, data):
 		if isinstance(data, date):
